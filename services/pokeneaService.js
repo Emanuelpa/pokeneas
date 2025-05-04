eee47fcad0a61988593b8bbda4f02993130f5194
Emanuelpa <epatinov@eafit.edu.co>
2025-05-04 11:01:14 -0500

    fix: get containerid

--- services/pokeneaService.js
+++ services/pokeneaService.js
@@ -1,21 +1,13 @@
 const fs = require('fs')
 const pokeneas = require('../data/pokeneas')
+const os = require("os")
 
 function getRandomPokenea() {
     return pokeneas[Math.floor(Math.random() * pokeneas.length)]
 }
 
 function getContainerId() {
-    try {
-        const cgroup = fs.readFileSync('/proc/self/cgroup', 'utf8')
-        const match = cgroup.match(/\/docker\/([a-f0-9]+)/)
-        if (match) {
-            return match[1].substring(0, 12)
-        }
-    } catch (err) {
-        console.error('Error al obtener ID del contenedor:', err.message)
-    }
-    return 'No disponible'
+    return os.hostname()
 }
 
 module.exports = {

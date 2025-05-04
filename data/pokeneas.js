2b4b0a8bb9e196dc98f0f5679db0d0ec7a9721d7
Emanuelpa <epatinov@eafit.edu.co>
2025-05-04 09:54:14 -0500

    fix: project structure

--- data/pokeneas.js
+++ data/pokeneas.js
@@ -4,7 +4,7 @@ const pokeneas = [
     nombre: 'Arepachu',
     altura: '1.2 m',
     habilidad: 'Lanzar arepas ardientes',
-    imagen: '/images/1.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/1.jpg',
     frase: '¡El que madruga, Dios lo ayuda!'
   },
   {
@@ -12,7 +12,7 @@ const pokeneas = [
     nombre: 'Silleon',
     altura: '1.5 m',
     habilidad: 'Ataque de silleta floral',
-    imagen: '/images/2.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/2.jpg',
     frase: '¡Pa’lante como el burro en la cuesta!'
   },
   {
@@ -20,7 +20,7 @@ const pokeneas = [
     nombre: 'Guaracachu',
     altura: '1.8 m',
     habilidad: 'Grito de feria',
-    imagen: '/images/3.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/3.jpg',
     frase: '¡Mucho ojo, pues!'
   },
   {
@@ -28,7 +28,7 @@ const pokeneas = [
     nombre: 'Mondongon',
     altura: '2.0 m',
     habilidad: 'Olor letal de mondongo',
-    imagen: '/images/4.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/4.jpg',
     frase: '¡Esto está más bueno que sancocho en leña!'
   },
   {
@@ -36,7 +36,7 @@ const pokeneas = [
     nombre: 'Cafecín',
     altura: '0.8 m',
     habilidad: 'Despierta a los rivales',
-    imagen: '/images/5.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/5.jpg',
     frase: '¡Con tinto y arepa todo se arregla!'
   },
   {
@@ -44,7 +44,7 @@ const pokeneas = [
     nombre: 'Chirimoyox',
     altura: '1.1 m',
     habilidad: 'Explosión frutal',
-    imagen: '/images/6.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/6.jpg',
     frase: '¡A lo bien, esta fruta es bendita!'
   },
   {
@@ -52,7 +52,7 @@ const pokeneas = [
     nombre: 'Parrandón',
     altura: '1.9 m',
     habilidad: 'Rumba interminable',
-    imagen: '/images/7.jpg',
+    imagen: 'https://pokeneast2-images.s3.us-east-1.amazonaws.com/7.jpg',
     frase: '¡El último que apague la luz!'
   }
 ]
